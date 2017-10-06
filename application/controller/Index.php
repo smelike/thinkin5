6b3be9688d47c15ca1446e6a45a65c84792bdc7a
jamesxu <jamesxujamesxu@jamesxus-Mac-mini.local>
2017-10-06 23:25:19 +0800

    use think\Db

--- application/controller/Index.php
+++ application/controller/Index.php
@@ -2,11 +2,19 @@
 
 namespace app\controller;
 
+use think\Db;
+
 class Index
 {
 	public function index()
 	{
 		echo __CLASS__ . __FUNCTION__;
 	}	
-
+	
+	public function test_mysql(){
+		$sql = "SELECT * FROM project";
+		$result = Db::query($sql);
+		
+		dump($result);
+	}
 }
