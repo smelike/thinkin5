85fb3abb0858acf4c29cfb66d02adae2f358dafd
jamesxu <jamesxujamesxu@jamesxus-Mac-mini.local>
2017-10-07 23:36:41 +0800

    vender view and output url in the view

--- application/controller/Index.php
+++ application/controller/Index.php
@@ -4,13 +4,24 @@ namespace app\controller;
 
 use think\Db;
 
-class Index
+class Index  extends \think\Controller
 {
 	public function index()
 	{
-		echo __CLASS__ . __FUNCTION__;
+		// find 只查找一条数据
+		//$projects = Db::table("project")->find();
+		$projects = Db::table("project")->select();
+		
+		$this->assign('projects', $projects);
+		return	$this->fetch('index');		
 	}	
 	
+	public function edit($id) {
+		var_dump($id);
+	}
+	public function delete($id) {
+		var_dump($id);
+	}
 	public function test_mysql(){
 		$sql = "SELECT * FROM project";
 		$result = Db::query($sql);
